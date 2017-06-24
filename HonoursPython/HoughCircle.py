9138cb0ddb64111b1efb0f372b33e7d8388aa144
MigGibson <miguel.gibson135@gmail.com>
2017-06-24 02:23:44 +0200

    HoughCircle update
    -GreyScale is giving an error.

--- HonoursPython/HoughCircle.py
+++ HonoursPython/HoughCircle.py
@@ -14,7 +14,7 @@ class HoughCircle:
             self.img = cv2.medianBlur(self.img, 5)
             
             #Grayscales image.
-            self.cimg = cv2.cvtColor(self.img, cv2.COLOR_GRAY2BGR)
+            cv2.cvtColor(self.img, self.cimg, cv2.COLOR_GRAY2BGR)
             
             #Identify the circles. HoughCircles(image, detection method, inverse ratio, min distance between detected centers, upper threshold, center threshold, min radius, max radius) 
             self.circles = cv2.HoughCircles(self.img, cv2.HOUGH_GRADIENT, 1, 5, 50, 30, 0, 0)
