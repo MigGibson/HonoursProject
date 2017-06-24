79ef9484bab334d54a55d2595d3bf1f2e498c52c
MigGibson <miguel.gibson135@gmail.com>
2017-06-24 19:45:00 +0200

    Test

--- HonoursPython/HoughCircle.py
+++ HonoursPython/HoughCircle.py
@@ -23,18 +23,22 @@ class HoughCircle:
             #Identify the circles. HoughCircles(image, detection method, inverse ratio, min distance between detected centers, upper threshold, center threshold, min radius, max radius) 
             #self.circles = cv2.HoughCircles(self.cimg, cv2.HOUGH_GRADIENT, 1, 20, 30, 15, 0, 0)
             #self.circles = cv2.HoughCircles(self.cimg, cv2.HOUGH_GRADIENT, 1, 20)
-            self.circles = cv2.HoughCircles(self.img, cv2.HOUGH_GRADIENT, 1, 20, 30, 15)
-            self.circles = np.uint16(np.around(self.circles))
+            self.circles = cv2.HoughCircles(self.img, cv2.HOUGH_GRADIENT, 1.2, 100)
             
-            #Display the circles.
-            for i in self.circles[0,:]:
-                #Draw outer circle.
-                #i[0] is x of center
-                #i[1] is y of center
-                #i[2] is radius
-                cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-            
-            #Display modified image.
-            cv2.imshow('detected circles',cimg)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            if self.circles is not None:
+                self.circles = np.uint16(np.around(self.circles))
+                
+                #Display the circles.
+                for i in self.circles[0,:]:
+                    #Draw outer circle.
+                    #i[0] is x of center
+                    #i[1] is y of center
+                    #i[2] is radius
+                    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+                
+                #Display modified image.
+                cv2.imshow('detected circles',cimg)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+            else:
+                print 'No circles found'
