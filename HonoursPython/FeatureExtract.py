af562901bfe74bd20d454bd1c1c0478db8ade9d2
MigGibson <miguel.gibson135@gmail.com>
2017-06-25 19:33:45 +0200

    Test

--- HonoursPython/FeatureExtract.py
+++ HonoursPython/FeatureExtract.py
@@ -1,7 +1,8 @@
-
+import cv2
+import numpy as np
 
 class FeatureExtract:
         
         #Constructor method takes in the image of the iris, and the circle (to get the center positions).
         def __init__(self, img, circle):
-            
+            print img.shape
