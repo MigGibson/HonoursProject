fb7cbfba8867543ff88b282b748a4107ec7af091
MigGibson <miguel.gibson135@gmail.com>
2017-06-25 23:53:58 +0200

    Added feature extraction
    -Extracts the iris in rings and outputs them into an image

--- HonoursPython/FeatureExtract.py
+++ HonoursPython/FeatureExtract.py
@@ -11,3 +11,29 @@ class FeatureExtract:
             print circle[1]
             print 'Radius: '
             print circle[2]
+            
+            outer_radius = circle[2]
+            
+            #Output array[radius][360 degrees].
+            #Our new feature extracted image.
+            output_image = np.zeros((outer_radius - 10, 360))
+            
+            #For each "row"/"ring"
+            for r in range(10, outer_radius):
+                #all 360 degrees around the center
+                for degree in range(0, 360):
+                    rad = np.radians(degree)
+                    #Get the x and y of each point on the ring at 360 degrees
+                    #Formulas are:
+                        #x = a + r*cos(theta)
+                        #y = b + r*sin(theta)
+                    new_x = circle[0] + (r * np.cos(rad))
+                    new_y = circle[1] + (r * np.sin(rad))
+                    
+                    #Add the new point to the output image.
+                    output_image[r][degree] = img[new_y][new_x]
+            
+            #Display modified image.
+            cv2.imshow('Feature Extraction', img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
