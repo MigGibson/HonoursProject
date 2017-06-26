9e9d049671e6afce1adc18316b25c4856cc1cfc3
MigGibson <miguel.gibson135@gmail.com>
2017-06-26 17:40:16 +0200

    Test

--- HonoursPython/FeatureExtract.py
+++ HonoursPython/FeatureExtract.py
@@ -32,7 +32,7 @@ class FeatureExtract:
                     
                     #print img[new_x][new_y]
                     #Add the new point to the output image.
-                    output_image[r - 10][degree] = img[new_x][new_y]
+                    output_image[r - 10][degree] = img[new_y][new_x]
                     #print output_image[r - 10][degree]
             
             output_image.astype('uint8')
