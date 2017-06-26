636a9227e0b846b9351cc1e4c4f3d7d80fd880fe
MigGibson <miguel.gibson135@gmail.com>
2017-06-26 22:03:17 +0200

    Test

--- HonoursPython/FeatureExtract.py
+++ HonoursPython/FeatureExtract.py
@@ -38,6 +38,6 @@ class FeatureExtract:
             
             self.rubber_output_image = np.array(output_image, dtype = np.uint8)
             #Display modified image.
-            cv2.imshow('Feature Extraction', self.histo_img)
+            cv2.imshow('Feature Extraction', self.rubber_output_image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
