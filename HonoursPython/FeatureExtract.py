57f0452f73a2a18525dfbd14fe2eebb73ac89121
MigGibson <miguel.gibson135@gmail.com>
2017-06-29 13:46:23 +0200

    Reduce outputs

--- HonoursPython/FeatureExtract.py
+++ HonoursPython/FeatureExtract.py
@@ -5,12 +5,12 @@ class FeatureExtract:
         
         #Constructor method takes in the image of the iris, and the circle (to get the center positions).
         def __init__(self, img, circle):
-            print 'Center(x): '
-            print circle[0]
-            print 'Center(y): '
-            print circle[1]
-            print 'Radius: '
-            print circle[2]
+            #print 'Center(x): '
+            #print circle[0]
+            #print 'Center(y): '
+            #print circle[1]
+            #print 'Radius: '
+            #print circle[2]
             
             outer_radius = circle[2]
             
