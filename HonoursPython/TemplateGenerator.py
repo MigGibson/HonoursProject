190b0afd1ccb74cd3eadeff09f3ab696bd4f0c70
MigGibson <miguel.gibson135@gmail.com>
2017-06-29 13:43:26 +0200

    Test

--- HonoursPython/TemplateGenerator.py
+++ HonoursPython/TemplateGenerator.py
@@ -10,15 +10,17 @@ class TemplateGenerator:
             #An array of size 8 to get the iris-code.
             self.code = np.zeros((0, 7))
             
-            print img.size
-            
             #Gabor filter.
-            #kern = cv2.getGaborKernel((height, 360), 4.0, theta, 10.0, 0.5, 0, ktype=cv2.CV_32F)
-            #kern /= 1.5*kern.sum()
+            kern = cv2.getGaborKernel((height, 360), 4.0, theta, 10.0, 0.5, 0, ktype=cv2.CV_32F)
+            kern /= 1.5*kern.sum()
             
-            #accum = np.zeros_like(img)
-            #fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
-            #np.maximum(accum, fimg, accum)
+            accum = np.zeros_like(img)
+            fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
+            np.maximum(accum, fimg, accum)
+            
+            cv2.imshow('Part', cropped_image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
             
             #Break up the image into 8 parts (45 degrees of the circle).
             for i in range(1, 8):
@@ -30,9 +32,9 @@ class TemplateGenerator:
                 #Cutting the image.
                 cropped_image = img[0:height - 1, prev:next]
                 
-                cv2.imshow('Part', cropped_image)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
+                #cv2.imshow('Part', cropped_image)
+                #cv2.waitKey(0)
+                #cv2.destroyAllWindows()
                 
                 #Store the code after letting the mean go through checks.
                 #self.code[i - 1] = 0
