789c0029730905fce81f7bdd715f8b74c0510125
MigGibson <miguel.gibson135@gmail.com>
2017-06-29 13:29:30 +0200

    Gabor Filter

--- HonoursPython/TemplateGenerator.py
+++ HonoursPython/TemplateGenerator.py
@@ -10,17 +10,25 @@ class TemplateGenerator:
             #An array of size 8 to get the iris-code.
             self.code = np.zeros((0, 7))
             
+            print img.size
+            
+            #Gabor filter.
+            #kern = cv2.getGaborKernel((, ksize), 4.0, theta, 10.0, 0.5, 0, ktype=cv2.CV_32F)
+            #kern /= 1.5*kern.sum()
+            
             #Break up the image into 8 parts (45 degrees of the circle).
             for i in range(1, 8)
                 
                 #Calculating the places to cut the image.
                 prev = (i - 1) * 45
-                next = (i) * 45
+                next = ((i) * 45) - 1
                 
                 #Cutting the image.
-                cropped_image = img[0:height, prev:next]
+                cropped_image = img[0:height - 1, prev:next]
                 
-                print (np.mean(cropped_image))
+                cv2.imshow('Part', cropped_image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
                 
                 #Store the code after letting the mean go through checks.
-                self.code[i - 1] = 0
+                #self.code[i - 1] = 0
