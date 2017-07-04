c86936dfa0b48225f0cb9857ef038fef11729700
MigGibson <miguel.gibson135@gmail.com>
2017-07-04 17:07:17 +0200

    Enrolment and User Management

--- IrisService/IrisService/IService1.cs
+++ IrisService/IrisService/IService1.cs
@@ -16,5 +16,53 @@ namespace IrisService
             ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.Wrapped)]
         int loginUser(String studentNum, String password);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/startEnrolment",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        bool startEnrolment();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/endEnrolment",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        bool endEnrolment();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/getEnrolmentStatus",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        bool getEnrolmentStatus();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/enrolUser/{studentNum}/{name}/{surname}/{password}/{type}/{cardUID}",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        void enrolUser(String studentNum, String name, String surname, String password, String type, String cardUID);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/enrolUserIris/{cardUID}/{irisHash}",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        void enrolUserIris(String cardUID, String irisHash);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/checkEnrolmentCompletion/{studentNum}",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string checkEnrolmentCompletion(String studentNum);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/doesUserExist/{studentNum}",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        bool doesUserExist(String studentNum);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/deactivateUser/{studentNum}",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        void deactivateUser(String studentNum);
     }
 }
--- IrisService/IrisService/Service1.svc.cs
+++ IrisService/IrisService/Service1.svc.cs
@@ -16,6 +16,9 @@ namespace IrisService
         private MySqlCommand cmd;
         private MySqlDataReader reader;
 
+        private bool Enrol = false;
+
+        //Login Processes
         public int loginUser(String studentNum, String password)
         {
             //Default value is being false.
@@ -47,6 +50,158 @@ namespace IrisService
             connection.Close();
             reader.Close();
             return result;
+        }
+
+        //Enrolment Processes
+        public bool startEnrolment()
+        {
+            this.Enrol = true;
+            return this.Enrol;
+        }
+
+        public bool endEnrolment()
+        {
+            this.Enrol = false;
+            return this.Enrol;
+        }
+
+        public bool getEnrolmentStatus()
+        {
+            return this.Enrol;
+        }
+
+        //Step 1:
+        //Enrols user with all their details.
+        public void enrolUser(String studentNum, String name, String surname, String password, String type, String cardUID)
+        {
+            DateTime date = DateTime.Now;
+
+            cmd = new MySqlCommand("INSERT INTO usertb VALUES ('@studentNum', '@pass', '@name', '@surname', '@type', '@card', '@date', 0);", connection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@studentNum", studentNum);
+            cmd.Parameters.AddWithValue("@pass", password);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@surname", surname);
+            cmd.Parameters.AddWithValue("@type", type);
+            cmd.Parameters.AddWithValue("@card", cardUID);
+            cmd.Parameters.AddWithValue("@date", date.Date);
+
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        //Step 2:
+        //Enrols the student's iris
+        public void enrolUserIris(String cardUID, String irisHash)
+        {
+            cmd = new MySqlCommand("INSERT INTO usertb VALUES ('@card', @irisHash);", connection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@card", cardUID;
+            cmd.Parameters.AddWithValue("@irisHash", irisHash);
+
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        //Check the current enrolment status of a user.
+        public string checkEnrolmentCompletion(String studentNum)
+        {
+            bool usertb = false;
+            bool iristb = false;
+            String cardUID = "";
+
+            //Checking if the user is in the user table.
+            cmd = new MySqlCommand("SELECT * FROM usertb WHERE StudentNumber = @studentNum AND Active = 0", connection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@studentNum", studentNum);
+            connection.Open();
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                //Return true because a match was found.
+                usertb = true;
+                cardUID = reader["Card_UID"].ToString();
+            }
+            connection.Close();
+            reader.Close();
+
+            if (cardUID == "")
+            {
+                return "No record of user!";
+            }
+
+            //Checking if the user is in the iris table.
+            cmd = new MySqlCommand("SELECT * FROM iristb WHERE Card_UID = @cardUID", connection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@cardUID", cardUID);
+            connection.Open();
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                //Return true because a match was found.
+                iristb = true;
+            }
+            connection.Close();
+            reader.Close();
+
+            if (usertb && iristb)
+            {
+                return "Enrolment Complete.";
+            }
+
+            if (usertb == true && iristb == false)
+            {
+                return "Details have been submitted. Awaiting iris enrolment.";
+            }
+
+            return "Unknown Error!";
+        }
+
+        //Attendance Process
+        public int takeAttendance(String cardUID, String irisHash)
+        {
+            return 0;
+        }
+
+        public int takeAttendance(String cardUID, String secretHash)
+        {
+            return 0;
+        }
+
+        //User Management
+        //Check to see if the user exists.
+        public bool doesUserExist(String studentNum)
+        {
+            cmd = new MySqlCommand("SELECT * FROM usertb WHERE StudentNumber = @studentNum AND Active=0", connection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@studentNum", studentNum);
+            connection.Open();
+
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                //Return true because a match was found.
+                return true;
+            }
+
+            connection.Close();
+            reader.Close();
+            return false;
+        }
+
+        //Called by admin.
+        public void deactivateUser(String studentNum)
+        {
+            cmd = new MySqlCommand("UPDATE usertb SET Active = 1 WHERE StudentNumber = @studentNum", connection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@studentNum", studentNum);
+            
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
         }
     }
 }
