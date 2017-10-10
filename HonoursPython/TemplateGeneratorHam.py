14bca357a22cd552676bea3fc4dc7e93abde5aab
MigGibson <miguel.gibson135@gmail.com>
2017-10-10 22:48:04 +0200

    Test

--- HonoursPython/TemplateGeneratorHam.py
+++ HonoursPython/TemplateGeneratorHam.py
@@ -94,12 +94,12 @@ class TemplateGeneratorHam:
                     #If ridge is not found
                     self.distance += 1
             
-            self.temp = np.array2string(self.code).replace(" ","_")
-            #self.temp = self.temp.replace("[","_")
+            self.temp = np.array2string(self.code).replace(" ","")
+            self.temp = self.temp.replace("[","")
             #self.temp = self.temp.replace("]","_")
-            #self.temp = self.temp.replace(".","_")
+            self.temp = self.temp.replace("."," ")
             #self.temp = self.temp.replace("_","")
-            #self.temp = self.temp.replace("\n","")
+            self.temp = self.temp.replace("\n","")
             
             print "The generated code: "
             print self.temp
